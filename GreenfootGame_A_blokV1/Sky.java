f95b2a8e2f5eb393e11e4d2b002d7b709a180677
aaikoosters <aaik.oosters@gmail.com>
2014-10-09 09:18:13 +0200

    Renamed
    - counters renamed
    - muur -> IndestructWall
    - DestructableWall own methode

--- GreenfootGame_A_blokV1/Sky.java
+++ GreenfootGame_A_blokV1/Sky.java
@@ -8,10 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Sky extends World
 {
-    private int counter1;
-    private int counter2;
+    private int counterIndestructableWalls;
+    private int counterDestructableWalls;
     int SPACE_BETWEEN_MUREN = 200;
-    //int hoogteRandom = Greenfoot.getRandomNumber(800) +300;
+       
     
   
     public Sky()
@@ -21,47 +21,54 @@ public class Sky extends World
         addObject( new Helicopter(), 400, 300 );
         addObject( new MovingWall(), 100, 100);
         setPaintOrder(IndestructableWall.class, DestructableWall.class);
-
-        
+               
     }
    
-    public static void getRandomNumber() {
-        int limit = 500;
-        int min = 300;
-        
-    }
-    
-    private void spawnWalls()
-
-    {
-       counter1++;
-       if (counter1 == 250) {
-           DownIndestructableWall downMuur = new DownIndestructableWall();           
-           GreenfootImage downImage = downMuur.getImage();
-
-           addObject(downMuur, getWidth(), getHeight()+ downImage.getHeight()-(Greenfoot.getRandomNumber(400) +300));    
-           
-           UpIndestructableWall upMuur = new UpIndestructableWall();
-           addObject(upMuur, getWidth(), downMuur.getY() - downImage.getHeight() -SPACE_BETWEEN_MUREN );
-           
-           addObject(downMuur, getWidth(), getHeight()+ downImage.getHeight()-(Greenfoot.getRandomNumber(400) +300));
-           counter1 = 0;
-        }
-        counter2++;
-       if (counter2 == 750) {
-           DestructableWall destruct = new DestructableWall();
-           GreenfootImage destructImage = destruct.getImage();
-           addObject(destruct, getWidth(), getHeight()/2);
-           
-           counter2 = 0;
-        }
-    }
-    
-    
     public void act()
     {
-        spawnWalls();
+        spawnIndestructableWalls();
+        spawnDestructableWalls();
     }
+    
+    private void spawnIndestructableWalls()
+
+    {
+       counterIndestructableWalls++;
+       if (counterIndestructableWalls == 250) {
+          
+           // Down Wall
+           DownIndestructableWall downMuur = new DownIndestructableWall();           
+           GreenfootImage downImage = downMuur.getImage();
+           addObject(downMuur, getWidth(), getHeight()+ downImage.getHeight()-(Greenfoot.getRandomNumber(400) +300));    
+           
+           // Up Wall
+           UpIndestructableWall upMuur = new UpIndestructableWall();
+           addObject(upMuur, getWidth(), downMuur.getY() - downImage.getHeight() -SPACE_BETWEEN_MUREN );
+                    
+           // Put counter to 0
+           counterIndestructableWalls = 0;
+           
+           
+           
+        }
+    }
+    
+    private void spawnDestructableWalls()
+    
+    {
+       counterDestructableWalls++;
+       if (counterDestructableWalls == 750) {
+           DestructableWall destruct = new DestructableWall();
+           GreenfootImage destructImage = destruct.getImage();
+           
+           addObject(destruct, getWidth(), getHeight()/2);
+           // Put counter to 0
+           counterDestructableWalls = 0;
+        }
+    }
+    
+    
+    
     
     
     /*    public static gameOver() 
